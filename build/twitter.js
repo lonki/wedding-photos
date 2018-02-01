31022f3a8198219b0a878ae3740d731933d76c29
lonki <snow76719@gmail.com>
2018-02-01 12:08:30 +0800

    twitter.js
    twitter.js

---
+++ build/twitter.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <meta property="og:title"       content="Style.me" />
+        <meta property="og:type"        content="website" />
+        <meta property="og:image"       content="https://beta.style.me/style-me/server-rendering?bodyId=0000000a10c221041000000000000000&skinTone=1&shoesColor=3&hairStyle=1&hairColor=2&avatarMeasurement=175_89_75_66_94&clothIds=00000dda1100210030001869e0800000-00000dc31100210040001869d0800000&greatFit=3%2C4&companyKeyId=4&brandId=f1031298-dcff-47a9-b77a-1d0300a859c3&brandKeyId=1&layeringOrders=2%2C1&viewMode=NORMAL&m=false&viewAngle=000000&addBackground=true&imageWidth=386&imageHeight=630" />
+        <meta property="og:image:width"  content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:site_name"   content="StyleMe" />
+        <meta name=“twitter:card” content=“summary_large_image”>
+        <meta property="og:description" content="Check out my latest look." />
+        <title>Share</title>
+        <script>window.location.href = 'https://test.style.me/sandbox?bodyId=0000000a10c221041000000000000000&skinTone=1&shoesColor=3&hairStyle=1&hairColor=2&avatarMeasurement=175_89_75_66_94&clothIds=00000de71100210030001869e1800000-00000de11100210030001869e1800000-00000dbc1100210040001869d1000000&greatFit=3%2C3%2C4&companyKeyId=4&brandId=&brandKeyId=1&layeringOrders=3%2C2%2C1&pids=3559%2C3553%2C3516&shareMediaType=twitter&smstc=6ea39c09-0d94-44e4-a683-9f4dcd5568af';</script>
+    </head>
+</html>
